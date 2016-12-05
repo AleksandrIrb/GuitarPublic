385951bd23f643ea1b0620fe3b1dc0c10fd4c4ec
Aleksandr Irbahtin <aleksandrgm163@mail.com>
2016-12-05 21:53:30 +0400

    Music Applications

---
+++ MusicGuitar/app/src/main/java/alex/aleksandr/ru/musicguitar/AboutAppActivity.java
@@ -0,0 +1,38 @@
+package alex.aleksandr.ru.musicguitar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class AboutAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_edit_close) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
